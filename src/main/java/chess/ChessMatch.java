ec31769e0a64a794f4cc42bd74481e66d7e6cadb
moreiraguui <guilhermemoreira111@hotmail.com>
2023-10-02 13:36:10 -0300

    Contagem de movimento de pecas

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -84,7 +84,8 @@ public class ChessMatch {
     }
 
     private Piece makeMove(Position source, Position target) {
-        Piece p = board.removePiece(source);
+        ChessPiece p =(ChessPiece) board.removePiece(source);
+        p.increaseMoveCount();
         Piece capturedPiece = board.removePiece(target);
         board.placePice(p, target);
 
@@ -96,7 +97,8 @@ public class ChessMatch {
     }
 
     private void undoMove(Position source, Position target, Piece capturedPiece) {
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece) board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePice(p, source);
 
         if (capturedPiece != null) {
@@ -105,6 +107,7 @@ public class ChessMatch {
             piecesOnTheBoard.add(capturedPiece);
         }
     }
+    
 
     private void validateSourcePosition(Position position) {
         if (!board.thereIsAPiece(position)) {

baa6e0d53abeab15ba1cc70981b2fe2635d3f0a2
moreiraguui <guilhermemoreira111@hotmail.com>
2023-10-05 16:32:09 -0300

    feat: melhoria no movimento especial Promotion

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -7,10 +7,8 @@ import chess.enums.Color;
 import chess.exception.ChessException;
 import chess.pieces.*;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class ChessMatch {
 
@@ -117,7 +115,7 @@ public class ChessMatch {
             throw new IllegalStateException("There is no piece to be promoted");
         }
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
